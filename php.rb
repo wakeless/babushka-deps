a0b0ef126be749c699d02a56861e21f18f0ba58d
Michael Gall <michael@wakeless.net>
2012-04-30 01:24:06 +1000

    jenkins stuffs

--- php.rb
+++ php.rb
@@ -32,24 +32,40 @@ end
 dep "git.jenkins"
 
 meta "jenkins" do
+  accepts_list_for :provides, :basename
+  def host
+    "localhost:8080"
+  end
+
   template {
+    before { 
+      log_shell "Updating plugins", "curl  -L http://updates.jenkins-ci.org/update-center.json | sed '1d;$d' | curl -X POST -H 'Accept: application/json' -d @- http://#{host}/updateCenter/byId/default/postBack" 
+    }
     met? {}
     meet { 
-      log_shell "Installing jenkins plugin #{basename}", "jenkins-cli -s http://localhost:8080 install-plugin #{basename} -restart"
+      log_shell "Installing jenkins plugin #{provides.join(" ")}", "jenkins-cli -s http://#{host} install-plugin #{basename} -restart"
     }
   }
 
 end
 
 dep "php jenkins" do
-  met? {}
+  def jenkins_jobs
+    "/var/lib/jenkins/jobs"
+  end
+
+  met? { File.exists? jenkins_jobs / "php-template/config.xml" }
   meet {
-    cd "/var/lib/jenkins/jobs", :create => true, :sudo => true do
+    cd jenkins_jobs, :create => true, :sudo => true do
       log_shell "Downloading sebastian bermann's jenkins templates", "git clone git://github.com/sebastianbergmann/php-jenkins-template.git php-template", :sudo => true
     end
     shell "invoke-rc.d jenkins stop", :sudo => true
     shell "invoke-rc.d jenkins start", :sudo => true
   }
+end
+
+dep "phpunit.jenkins" do
+  provides "checkstyle", "cloverphp", "dry", "htmlpublisher", "jdepend", "plot", "pmd", "violations", "xunit"
 end
 
 meta "pear", :version do
