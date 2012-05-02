6e40c7bec141eafd77a83ad25b4cae6f84eb2ebe
Michael Gall <michael@wakeless.net>
2012-05-03 00:21:07 +1000

    jenkins fixes

--- jenkins.rb
+++ jenkins.rb
@@ -11,7 +11,8 @@ dep "jenkins git job", :job_name, :git_name, :git_url, :email_notification do
   meet { 
     cd "/tmp" do
       render_erb "jenkins/job.xml.erb", :to => "/tmp/config.xml", :comment => "<!-- ", :comment_suffix => " -->" 
-      shell "jenkins-cli create-job #{job_name} < config.xml"
+      shell "tail -n +2 config.xml > config.xml"
+      shell "jenkins-cli create-job "#{job_name}" < config.xml"
     end
   }
 end
