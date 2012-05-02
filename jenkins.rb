0d1700f1027fb2966a55d0eb2d0c1b23626df562
Michael Gall <michael@wakeless.net>
2012-05-02 21:41:25 +1000

    Adding jenkins job capabilities

--- jenkins.rb
+++ jenkins.rb
@@ -6,12 +6,24 @@ dep "jenkins" do
   requires "jenkins.managed", "git.jenkins"
 end
 
+dep "jenkins git job", :name, :git_name, :git_url, :email_notification do
+  met? { File.exists? "/var/lib/jenkins/jobs/#{name}/config.xml" }
+  meet { 
+    cd "/tmp" do
+      render_erb "jenkins/job.xml.erb", :to => "config.xml"
+      shell "jenkins-cli create-job #{name} < config.xml"
+    end
+  }
+end
+
 dep "git.jenkins" do
   after {
     cd home / ".ssh", :sudo => true, :create => true do
       sudo "ssh-keygen -f id_rsa"
       sudo "chown -R jenkins:jenkins .ssh"
       sudo "chmod -R 600 ."
+      log "Here's the public key..."
+      log exec("cat .ssh/id_rsa.pub")
     end
   }
 end
