c3bdf00c3f659ea9c2dc5180ade320b0f98724cc
Michael Gall <michael@wakeless.net>
2012-06-22 14:42:59 +1000

    Checkin for starting an ec2 instance

--- ec2.rb
+++ ec2.rb
@@ -11,3 +11,55 @@ dep "mount-ebs.upstart", :device, :volume, :mount_path do
   end
 
 end
+
+
+dep "aws-sdk.gem" do
+  provides ""
+end
+
+dep "start.ec2", :id, :region do
+  
+  def instance
+    ec2.instances[id]
+  end
+
+  met? { instance.exists? and [:running, :pending].include? instance.status }
+  meet { ec2.instances[id].start }
+  
+end
+
+meta "ec2" do
+  template {
+    def ec2
+      unless @ec2
+        configure
+        @ec2 = AWS::EC2.new
+        @ec2 = @ec2.regions["us-west-1"]
+      end
+      @ec2
+    end
+
+    requires "aws-sdk.gem", "ec2 credentials"
+
+    require "aws-sdk"
+    @config = "~" / ".ec2/config.yml"
+
+    def configure
+      AWS.config(YAML.load(File.read(@config)))
+    end
+    
+  }
+
+end
+
+dep "ec2 credentials", :access_key_id, :secret_access_key do
+  @config = "~" / ".ec2/config.yml"
+  met? {
+    @config.exists?
+  }
+  meet {
+    @config.write "
+access_key_id: #{access_key_id}
+secret_access_key: #{secret_access_key}"
+  }
+end
