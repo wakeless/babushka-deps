6bf9b2a71456e0020f748ee0b2ef44238c1f1c50
Michael Gall <michael@wakeless.net>
2012-06-25 20:55:29 +1000

    Adding dynds stuffs

---
+++ dyndns.rb
@@ -0,0 +1,12 @@
+dep "ddclient.managed"
+
+dep "dnsdynamic", :username, :password, :domain do
+  #requires "ddclient.managed"
+
+  def config
+    "/etc" / "ddclient.conf"
+  end
+
+  met? { config.exists? }
+  meet { render_erb "dyndns/ddclient.erb", :to => config, :sudo => true, :chmod => 600 }
+end
